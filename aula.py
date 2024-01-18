8878d336d6c449c6e46f28b31a43a5429586bf97
alexandre <alexandre.afonso@prozeducacao.com.br>
2024-01-17 21:26:26 -0300

    Exercio de python

---
+++ aula.py
@@ -0,0 +1,13 @@
+nome = input("Digite o nome do produto")
+preco = int(input("Digite o preço"))
+
+if(preco < 0):
+    print(f"Esse não é um preço valido")
+elif(preco <= 50):
+    print(f"o {nome} tem um valor considerado baixo. O Valor dele é {preco} reais")
+elif(preco <= 100):
+    print(f"O {nome} tem um valor considerado médio. O Valor dele é {preco} reais")
+else:
+    print(f"O {nome} tem um valor considerado Alto. O Valor dele é {preco} reais")
+
+
